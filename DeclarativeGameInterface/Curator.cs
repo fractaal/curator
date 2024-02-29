e05004e8ebbcc1891343db5c896e1827af4523b4
Ben <sonoroustwo@gmail.com>
2024-03-01 02:10:17 +0800

    Add LLMInterface and load prompt from file

--- DeclarativeGameInterface/Curator.cs
+++ DeclarativeGameInterface/Curator.cs
@@ -3,5 +3,19 @@ using Godot;
 
 public partial class Curator : Node
 {
-    // WIP
+    [Export]
+    private LLMInterface llmInterface;
+
+    private string PROMPT = "";
+
+    public Curator()
+    {
+        var file = FileAccess.Open(
+            "res://DeclarativeGameInterface/prompts/Main.txt",
+            FileAccess.ModeFlags.Read
+        );
+        PROMPT = file.GetAsText();
+    }
+
+    private void _ready() { }
 }
