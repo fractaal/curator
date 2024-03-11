3ec7d40439bb20f22841b2d71617779bd2f00d63
Ben <benedict.jude.rocat@gmail.com>
2024-03-11 17:44:18 +0800

    Refactor code to improve performance and readability

--- DeclarativeGameInterface/Interpreter.cs
+++ DeclarativeGameInterface/Interpreter.cs
@@ -4,236 +4,201 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using Godot;
 
-public struct AbstractSyntaxTreeNode
+public struct Command
 {
-	public string verb;
-	public List<string> arguments;
-}
-
-public class Token
-{
-	public string Type { get; private set; }
-	public string Value { get; private set; }
-
-	public Token(string type, string value)
-	{
-		Type = type;
-		Value = value;
-	}
-
-	public override string ToString()
-	{
-		return $"Token({Type}, {Value})";
-	}
+    public string Verb;
+    public List<string> Arguments;
 }
 
 public partial class Interpreter : Node
 {
-	private Node3D player = null;
-	private Node3D ghost = null;
+    private Node3D player = null;
+    private Node3D ghost = null;
 
-	private void _ready()
-	{
-		player = (Node3D)GetTree().CurrentScene.FindChild("Player");
-		ghost = (Node3D)GetTree().CurrentScene.FindChild("Ghost");
+    private void _ready()
+    {
+        player = (Node3D)GetTree().CurrentScene.FindChild("Player");
+        ghost = (Node3D)GetTree().CurrentScene.FindChild("Ghost");
 
-		GD.Print("Initialized interpreter with values player: ", player, " ghost: ", ghost);
-	}
+        GD.Print("Initialized interpreter with values player: ", player, " ghost: ", ghost);
+    }
 
-	private Dictionary<string, string> tokenTypes = new Dictionary<string, string>
-	{
-		{
-			"VERB",
-			@"(moveTo|emitSound|manifest|speakAsGhost|speakAsSpiritBox|toggleLights|turnOffLights|flickerLights|explodeLights|restoreLights|toggleTVs|flickerTVs|explodeTVs|ringPhones|throwPhysicsObject|knockWindows|knockDoors|openDoors|closeDoors|lockDoors)"
-		},
-		{ "MODIFIER", @"(at|random|all|in)" },
-		{ "ENTITY", @"(player|ghost|room)" },
-		{ "NAME", @"[a-zA-Z][a-zA-Z0-9]*" },
-		{ "SPACE", @"\s+" },
-		{ "LPAREN", @"\(" },
-		{ "RPAREN", @"\)" }
-	};
+    private List<string> objectInteractionVerbPrefixes =
+        new() { "flicker", "explode", "restore", "turnOff" };
 
-	private List<string> objectInteractionVerbPrefixes =
-		new() { "flicker", "explode", "restore", "turnOff" };
+    private string accumulatedText = "";
 
-	public List<Token> Lexer(string code)
-	{
-		List<Token> tokens = new List<Token>();
-		while (!string.IsNullOrEmpty(code))
-		{
-			bool match = false;
-			foreach (var tokenType in tokenTypes)
-			{
-				Match regexMatch = Regex.Match(code, "^" + tokenType.Value);
-				if (regexMatch.Success)
-				{
-					match = true;
-					if (tokenType.Key != "SPACE")
-					{
-						tokens.Add(new Token(tokenType.Key, regexMatch.Value));
-					}
-					code = code.Substring(regexMatch.Length);
-					break;
-				}
-			}
+    public List<Command> Parse(string chunk)
+    {
+        // Matches stuff like verb(), verb(arg1), verb(arg1, arg2)...
+        var pattern = @"\w+\((\w+)?(,\s*\w+)?\)";
 
-			if (!match)
-			{
-				throw new ArgumentException($"Illegal character: {code[0]}");
-			}
-		}
-		return tokens;
-	}
+        var matches = Regex.Matches(chunk, pattern);
 
-	public List<AbstractSyntaxTreeNode> Parser(List<Token> tokens)
-	{
-		List<AbstractSyntaxTreeNode> ast = new();
-		while (tokens.Count > 0)
-		{
-			Token verbToken = tokens[0];
-			tokens.RemoveAt(0);
-			if (verbToken.Type != "VERB")
-			{
-				throw new ArgumentException("Command must start with a verb");
-			}
+        if (matches.Count() > 0)
+        {
+            var recognizedCommands = new List<Command>();
 
-			if (tokens.Count == 0 || tokens[0].Type != "LPAREN")
-			{
-				throw new ArgumentException("Expected '(' after verb");
-			}
-			tokens.RemoveAt(0);
+            foreach (string match in matches)
+            {
+                var separatedString = match.Split("(");
+                var verb = separatedString[0];
 
-			List<string> arguments = new List<string>();
-			while (tokens.Count > 0 && tokens[0].Type != "RPAREN")
-			{
-				Token token = tokens[0];
-				tokens.RemoveAt(0);
-				if (token.Type == "MODIFIER" || token.Type == "ENTITY" || token.Type == "NAME")
-				{
-					arguments.Add(token.Value);
-				}
-				else
-				{
-					throw new ArgumentException("Unexpected token type in arguments");
-				}
-			}
+                var argumentString = separatedString[1].Substring(0, separatedString[1].Length - 1);
+                var arguments = argumentString
+                    .Split(",")
+                    .Select(argument => argument.Trim())
+                    .ToList();
 
-			if (tokens.Count == 0 || tokens[0].Type != "RPAREN")
-			{
-				throw new ArgumentException("Expected ')' after arguments");
-			}
-			tokens.RemoveAt(0);
+                recognizedCommands.Add(new Command { Verb = verb, Arguments = arguments });
+            }
 
-			ast.Add(new() { verb = verbToken.Value, arguments = arguments, });
-		}
+            return recognizedCommands;
+        }
+        else
+        {
+            accumulatedText += chunk;
+            return new() { };
+        }
+    }
 
-		return ast;
-	}
+    public void Interpret(string chunk)
+    {
+        List<Command> commands = Parse(chunk);
 
-	public void Interpret(string code)
-	{
-		List<Token> tokens = Lexer(code);
-		List<AbstractSyntaxTreeNode> ast = Parser(tokens);
-		foreach (AbstractSyntaxTreeNode node in ast)
-		{
-			GD.Print(node.verb);
+        foreach (Command command in commands)
+        {
+            GD.Print(command.Verb);
 
-			var searchSpace = getObjectSearchSpace(node.arguments);
+            var searchSpace = getObjectSearchSpace(command.Arguments);
 
-			string prefix = null;
+            string prefix = null;
 
-			try
-			{
-				prefix = objectInteractionVerbPrefixes.First(p =>
-				{
-					var res = node.verb.Contains(p);
-					GD.Print(node.verb, " contains ", p, "? - ", res);
-					return res;
-				});
-			}
-			catch (Exception e)
-			{
-				GD.Print(node.verb, " did not have a valid object interaction verb prefix");
-			}
+            try
+            {
+                prefix = objectInteractionVerbPrefixes.First(prefix =>
+                {
+                    var result = command.Verb.Contains(prefix);
 
-			if (prefix != null)
-			{
-				GD.Print("was object interaction command!");
-				foreach (Node3D n in searchSpace)
-				{
-					n.Call(prefix);
-				}
-			}
-		}
-	}
+                    GD.Print("Object interaction prefix detected (", command.Verb, ")");
 
-	private List<Node3D> getObjectSearchSpace(List<string> arguments)
-	{
-		int inKeywordIndex = arguments.IndexOf("in");
-		string roomName = null;
+                    return result;
+                });
+            }
+            catch (Exception e)
+            {
+                GD.Print(command.Verb, " did not have a valid object interaction prefix");
+            }
 
-		var lights = GetTree().GetNodesInGroup("lights").Cast<Node3D>().ToList();
-		var tvs = GetTree().GetNodesInGroup("tvs").Cast<Node3D>().ToList();
+            if (prefix != null)
+            {
+                GD.Print("Was object interaction command!");
 
-		if (inKeywordIndex != -1)
-		{
-			roomName = arguments[inKeywordIndex + 1];
-		}
+                foreach (Node3D node in searchSpace)
+                {
+                    node.Call(prefix); // Convention.
+                }
+            }
+        }
+        // foreach (AbstractSyntaxTreeNode node in ast)
+        // {
+        //     GD.Print(node.verb);
 
-		if (roomName == null)
-		{
-			List<Node3D> nodes = new();
+        //     var searchSpace = getObjectSearchSpace(node.arguments);
 
-			nodes.AddRange(lights);
-			nodes.AddRange(tvs);
+        //     string prefix = null;
 
-			return nodes;
-		}
+        //     try
+        //     {
+        //         prefix = objectInteractionVerbPrefixes.First(p =>
+        //         {
+        //             var res = node.verb.Contains(p);
+        //             GD.Print(node.verb, " contains ", p, "? - ", res);
+        //             return res;
+        //         });
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         GD.Print(node.verb, " did not have a valid object interaction verb prefix");
+        //     }
 
-		var rooms = GetTree().GetNodesInGroup("rooms");
+        //     if (prefix != null)
+        //     {
+        //         GD.Print("was object interaction command!");
+        //         foreach (Node3D n in searchSpace)
+        //         {
+        //             n.Call(prefix);
+        //         }
+        //     }
+        // }
+    }
 
-		Area3D selectedRoom = null;
+    private List<Node3D> getObjectSearchSpace(List<string> arguments)
+    {
+        int inKeywordIndex = arguments.IndexOf("in");
+        string roomName = null;
 
-		for (int i = 0; i < rooms.Count; i++)
-		{
-			if (rooms[i].Name.ToString().ToLower().Contains(roomName.ToLower()))
-			{
-				selectedRoom = (Area3D)rooms[i];
-				break;
-			}
-		}
+        var lights = GetTree().GetNodesInGroup("lights").Cast<Node3D>().ToList();
+        var tvs = GetTree().GetNodesInGroup("tvs").Cast<Node3D>().ToList();
 
-		if (selectedRoom == null)
-		{
-			GD.Print(roomName, " apparently does not exist. Defaulting to all");
-			List<Node3D> nodes = new();
+        if (inKeywordIndex != -1)
+        {
+            roomName = arguments[inKeywordIndex + 1];
+        }
 
-			nodes.AddRange(lights);
-			nodes.AddRange(tvs);
-			return nodes;
-		}
+        if (roomName == null)
+        {
+            List<Node3D> nodes = new();
 
-		var roomNodes = selectedRoom.GetOverlappingBodies().ToList();
+            nodes.AddRange(lights);
+            nodes.AddRange(tvs);
 
-		List<Node3D> validNodes = new();
+            return nodes;
+        }
 
-		foreach (Node3D roomNode in roomNodes)
-		{
-			Node currentNode = roomNode;
+        var rooms = GetTree().GetNodesInGroup("rooms");
 
-			while (currentNode.GetParent() != null)
-			{
-				if (currentNode.IsInGroup("lights") || currentNode.IsInGroup("tvs"))
-				{
-					validNodes.Add((Node3D)currentNode);
-				}
-				currentNode = currentNode.GetParent();
-			}
-		}
+        Area3D selectedRoom = null;
 
-		validNodes.ForEach(n => GD.Print(n.Name));
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].Name.ToString().ToLower().Contains(roomName.ToLower()))
+            {
+                selectedRoom = (Area3D)rooms[i];
+                break;
+            }
+        }
 
-		return validNodes;
-	}
+        if (selectedRoom == null)
+        {
+            GD.Print(roomName, " apparently does not exist. Defaulting to all");
+            List<Node3D> nodes = new();
+
+            nodes.AddRange(lights);
+            nodes.AddRange(tvs);
+            return nodes;
+        }
+
+        var roomNodes = selectedRoom.GetOverlappingBodies().ToList();
+
+        List<Node3D> validNodes = new();
+
+        foreach (Node3D roomNode in roomNodes)
+        {
+            Node currentNode = roomNode;
+
+            while (currentNode.GetParent() != null)
+            {
+                if (currentNode.IsInGroup("lights") || currentNode.IsInGroup("tvs"))
+                {
+                    validNodes.Add((Node3D)currentNode);
+                }
+                currentNode = currentNode.GetParent();
+            }
+        }
+
+        validNodes.ForEach(n => GD.Print(n.Name));
+
+        return validNodes;
+    }
 }
