8eaf22a54289c413ca8490667e0e1a86489d2276
Ben <sonoroustwo@gmail.com>
2024-03-29 00:47:58 +0800

    Add EventBus class for event handling

---
+++ Scripts/EventBus.cs
@@ -0,0 +1,55 @@
+using Godot;
+
+public partial class EventBus : Node
+{
+    [Signal]
+    public delegate void ObjectInteractionEventHandler(
+        string verb,
+        string objectType,
+        string target
+    );
+
+    [Signal]
+    public delegate void GhostActionEventHandler(string verb, string arguments);
+
+    [Signal]
+    public delegate void LogUpdatedEventHandler(string id, string message);
+
+    [Signal]
+    public delegate void GameDataReadEventHandler(string data);
+
+    [Signal]
+    public delegate void LLMPromptedEventHandler(string prompt);
+
+    [Signal]
+    public delegate void LLMFirstResponseChunkEventHandler(string chunk);
+
+    [Signal]
+    public delegate void LLMResponseChunkEventHandler(string chunk);
+
+    [Signal]
+    public delegate void LLMLastResponseChunkEventHandler(string chunk);
+
+    [Signal]
+    public delegate void InterpreterCommandRecognizedEventHandler(string command);
+
+    [Signal]
+    public delegate void NotableEventOccurredEventHandler(string message);
+
+    private static EventBus instance;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        instance = this;
+    }
+
+    public static EventBus Get()
+    {
+        if (instance == null)
+        {
+            throw new System.Exception("EventBus should be an autoload");
+        }
+        return instance;
+    }
+}
