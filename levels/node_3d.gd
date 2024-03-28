fbe2e7a3c4165e15a14d1e220c7ae439c0f38cb2
Ben <sonoroustwo@gmail.com>
2024-03-29 00:42:50 +0800

    Remove commented out code in node_3d.gd

--- levels/node_3d.gd
+++ levels/node_3d.gd
@@ -2,27 +2,3 @@ extends Node3D
 
 @onready var player = $Player
 @onready var rng = RandomNumberGenerator.new()
-
-func _ready():
-	pass
-	# var lastCommand;
-
-	# while true:
-	# 	await get_tree().create_timer(rng.randf_range(2,5)).timeout
-
-	# 	var chance = randf();
-
-	# 	if lastCommand == "explodeLights(all)":
-	# 		lastCommand = "restoreLights(all)"
-	# 		Curator.Interpret("restoreLights(all)")
-	# 		continue
-
-	# 	if chance > 0.95:
-	# 		lastCommand = "explodeLights(all)"
-	# 		Curator.Interpret("explodeLights(all)")
-	# 	elif chance > 0.5:
-	# 		lastCommand = "flickerLights(all)"
-	# 		Curator.Interpret("flickerLights(all)")
-
-
-
