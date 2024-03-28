d4846e7b07959aa74dc88265cb2c0b994ccf0802
Ben <sonoroustwo@gmail.com>
2024-03-29 00:49:02 +0800

    Add TargetResolution script for resolving targets

---
+++ Scripts/TargetResolution.cs
@@ -0,0 +1,59 @@
+using Godot;
+
+public partial class TargetResolution : Node
+{
+    private static TargetResolution instance;
+
+    public override void _Ready()
+    {
+        instance = this;
+    }
+
+    public static Node GetTarget(string target)
+    {
+        target = target.Trim().ToLower();
+
+        if (target == "player")
+        {
+            return instance.GetTree().CurrentScene.FindChild("Player");
+        }
+        else if (target == "ghost")
+        {
+            return instance.GetTree().CurrentScene.FindChild("Ghost");
+            // Otherwise assume it's a room
+        }
+        else
+        {
+            if (target.StartsWith("in"))
+            {
+                target = target.Substring(2).Trim();
+            }
+
+            var rooms = instance.GetTree().GetNodesInGroup("rooms");
+
+            foreach (Node room in rooms)
+            {
+                if (room.Name.ToString().ToLower().Contains(target))
+                {
+                    return room;
+                }
+            }
+        }
+
+        throw new System.Exception("Target not found: " + target);
+    }
+
+    public static Vector3 GetTargetPosition(string target)
+    {
+        var node = GetTarget(target);
+
+        if (node is Node3D)
+        {
+            return ((Node3D)node).GlobalTransform.Origin;
+        }
+        else
+        {
+            return new Vector3();
+        }
+    }
+}
