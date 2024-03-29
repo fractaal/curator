df21aee5db78ff4f039e82692f1b707927bee47e
Ben <sonoroustwo@gmail.com>
2024-03-30 04:00:24 +0800

    Add Interactable scanning functionality to Room class

--- levels/Room.cs
+++ levels/Room.cs
@@ -10,9 +10,39 @@ public partial class Room : Area3D
     public static event Action<Room, Node3D> RoomEntered;
     public static event Action<Room, Node3D> RoomExited;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public List<Node> Interactables { get; private set; }
+
+    public Node findInteractableNode(Node node)
     {
+        var parent = node;
+
+        while (parent != null)
+        {
+            if (parent.IsInGroup("interactables"))
+            {
+                if (parent.HasNode("Interactable"))
+                {
+                    return parent.GetNode("Interactable");
+                }
+                else
+                {
+                    throw new Exception(
+                        "Interactable node found but no Interactable script attached"
+                    );
+                }
+            }
+            ;
+            parent = parent.GetParent();
+        }
+
+        throw new Exception("No interactable node found");
+    }
+
+    // Called when the node enters the scene tree for the first time.
+    public async override void _Ready()
+    {
+        Interactables = new();
+
         Monitoring = true;
         BodyEntered += (body) =>
         {
@@ -37,6 +67,64 @@ public partial class Room : Area3D
             }
             catch (Exception) { }
         };
+
+        // Get all interactables in the room
+        // wait a second because scene tree may not be ready
+
+        await ToSignal(GetTree().CreateTimer(2), "timeout");
+
+        var bodies = GetOverlappingBodies();
+
+        foreach (Node body in bodies)
+        {
+            try
+            {
+                var interactable = findInteractableNode(body);
+
+                if (interactable != null)
+                {
+                    Interactables.Add(interactable);
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+
+    public string GetInformation()
+    {
+        string info = "";
+
+        info += $"- {Name}\n";
+
+        foreach (Node interactable in Interactables)
+        {
+            try
+            {
+                if (interactable.HasMethod("getStatus"))
+                {
+                    string line = interactable.Call("getStatus").AsString();
+
+                    if (line == "")
+                    {
+                        continue;
+                    }
+
+                    info += "\t" + line + "\n";
+                }
+                else
+                {
+                    GD.PushWarning(
+                        "Interactable " + interactable.GetParent().Name + " has no getStatus method"
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                GD.PushWarning("Error getting interactable status: " + e.Message);
+            }
+        }
+
+        return info.Trim();
     }
 
     public static List<Room> GetRooms()
