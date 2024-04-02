fdf08a122943ee846180f519337b8b0357471c75
Ben <benedict.jude.rocat@gmail.com>
2024-04-02 18:31:41 +0800

    Update ShadowMovement speed and textures

--- Scenes/Evidences/ShadowMovement.gd
+++ Scenes/Evidences/ShadowMovement.gd
@@ -1,23 +1,51 @@
 extends CharacterBody3D
 
-var speed = 2.5
+var speed = 5
 
 @onready var nav_agent = $NavigationAgent3D
-@onready var skeleton: Node3D = $Skeleton3D
 @onready var CollisionShape = $CollisionShape3D
 @export var Locator: Node
 
-func _ready():
-	while true:
-		await get_tree().create_timer(randf_range(5, 10)).timeout
-
-		update_target_location(Locator.RoomObject.GetRandomPosition())
+@export var textures: Array[CompressedTexture2D]
 
 var last_location: Vector3
+var current_location: Vector3
+var next_location: Vector3
+
+var rooms;
+
+var maxIters = 3
+var iters = 0
+
+func _ready():
+	($CollisionShape3D/Sprite3D as Sprite3D).texture = textures[randi() % textures.size()]
+
+	while true:
+		iters += 1
+		await get_tree().create_timer(randf_range(5, 10)).timeout
+
+		var tween = create_tween()
+
+		tween.tween_property($CollisionShape3D/Sprite3D, "modulate", Color(1, 1, 1, 1), 0.5)
+
+		tween.play()
+
+		if Locator.RoomObject:
+			update_target_location(Locator.RoomObject.GetRandomPosition())
+
+		await get_tree().create_timer(1).timeout
+
+		tween = create_tween()
+
+		tween.tween_property($CollisionShape3D/Sprite3D, "modulate", Color(1, 1, 1, 0), 0.5)
+		tween.play()
+
+		if iters >= maxIters:
+			break
 
 func _physics_process(delta):
-	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_path_position()
+	current_location = global_transform.origin
+	next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * speed
 
 	if (current_location - next_location).length() < 0.1:
@@ -32,8 +60,5 @@ func _physics_process(delta):
 	
 	last_location = current_location
 
-func _process(_delta):
-	pass
-	
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
