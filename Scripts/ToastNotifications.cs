ddea4f5e8ca931a13cf2302f0981d50a191ebcb2
Ben <sonoroustwo@gmail.com>
2024-04-08 04:43:06 +0800

    Add ToastNotifications class for displaying toast notifications
    Former-commit-id: 9c446cddf626a92a087508f3db09a42f5fcdd1f5

---
+++ Scripts/ToastNotifications.cs
@@ -0,0 +1,34 @@
+using System;
+using Godot;
+
+public partial class ToastNotifications : Node
+{
+    [Export]
+    private Label StatusView;
+
+    private EventBus bus;
+
+    Tween tween;
+
+    public override void _Ready()
+    {
+        StatusView = GetTree().CurrentScene.GetNode<Label>("CenterContainer/VBoxContainer/Label");
+        bus = EventBus.Get();
+
+        bus.ToastNotification += (message) =>
+        {
+            StatusView.Text = message;
+
+            if (tween != null)
+            {
+                tween.Stop();
+            }
+
+            tween = CreateTween();
+
+            StatusView.Modulate = new Color(1, 1, 1, 1);
+            tween.TweenProperty(StatusView, "modulate", new Color(1, 1, 1, 0), 1);
+            tween.Play();
+        };
+    }
+}
