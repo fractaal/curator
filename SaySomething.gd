5315cdeab1ab4b6ec497c043d75260f081e6ac15
Ben <sonoroustwo@gmail.com>
2024-04-08 04:43:53 +0800

    Refactor update_text function and add text parameter
    Former-commit-id: 6b802b56b7946984ace4d1f01c0bbf85571ec11a

--- SaySomething.gd
+++ SaySomething.gd
@@ -24,13 +24,9 @@ func _input(event):
 var completed_text := ""
 var partial_text := ""
 
-func update_text():
+func update_text(_text):
 	# This method might no longer be necessary depending on your use case
-	placeholder_text = completed_text + partial_text
-
-func _process(_delta):
-	# Depending on how your game's logic works, you might not need to call update_text here anymore
-	update_text()
+	placeholder_text = _text
 
 func _on_capture_stream_to_text_transcribed_msg(is_partial, new_text):
 	if is_partial:
@@ -42,10 +38,13 @@ func _on_capture_stream_to_text_transcribed_msg(is_partial, new_text):
 			EventBus.emit_signal("NotableEventOccurred", "Player said: \"" + new_text.strip_edges() + "\"")
 			completed_text = "" # Reset the completed_text since it's already handled
 			partial_text = "" # Clear partial text as well
+			update_text("You said - " + new_text)
+			EventBus.emit_signal("ToastNotification", "You said \"" + new_text + "\"")
 		else:
 			# If it's not a complete sentence, just update the partial_text
 			partial_text = new_text
+			update_text("You're saying - " + completed_text + partial_text)
 
-func _is_complete_sentence(text):
+func _is_complete_sentence(t):
 	# Check if the text ends with '.', '!', or '?' and is longer than 6 characters, and not just ellipses
-	return text.length() > 6 and (text.ends_with(".") or text.ends_with("!") or text.ends_with("?")) and not text.strip_edges().begins_with(".")
+	return t.length() > 6 and (t.ends_with(".") or t.ends_with("!") or t.ends_with("?")) and not t.strip_edges().begins_with(".")
