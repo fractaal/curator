ff036e6febd4aafd4bdc005559babd94d3a9d1d3
Ben <sonoroustwo@gmail.com>
2024-04-08 04:34:51 +0800

    Add ControlsUI script for handling UI animation
    Former-commit-id: e193833f5c704b1b1fd1e3e125d59ed77464a99d

---
+++ levels/ControlsUI.gd
@@ -0,0 +1,21 @@
+extends RichTextLabel
+
+var tween: Tween
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ShowHelp"):
+		animate()
+			
+func _ready():
+	animate()
+
+func animate():
+	if tween:
+		tween.stop()
+	
+	tween = create_tween()
+	
+	modulate = Color(1, 1, 1, 1)
+	tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 1).set_delay(5)
+	
+	tween.play()
