3ad69d176afc16395fbcd8fa1a28b7dd91f40cf2
Ben <benedict.jude.rocat@gmail.com>
2024-04-08 19:16:26 +0800

    Update platform-specific graphics settings in node_3d.gd

--- levels/node_3d.gd
+++ levels/node_3d.gd
@@ -1,4 +1,17 @@
 extends Node3D
 
-@onready var player = $Player
-@onready var rng = RandomNumberGenerator.new()
+func _ready():
+	var platform := OS.get_name()
+	
+	print("Platform is ", platform)
+	
+	var environment: WorldEnvironment = $WorldEnvironment
+	
+	if platform == "macOS":
+		print("Low-power machine detected, disabling graphics features")
+		DisplayServer.window_set_size(Vector2i(960,540))
+		
+		environment.environment.sdfgi_enabled = false
+		environment.environment.volumetric_fog_enabled = false
+		environment.environment.ssao_enabled = false
+		
