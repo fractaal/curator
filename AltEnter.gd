b4d5afe2f7dee754a4851e0974dca52246bd9ef8
Ben <sonoroustwo@gmail.com>
2024-04-11 13:48:07 +0800

    Add AltEnter.gd file for fullscreen toggle functionality

---
+++ AltEnter.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _input(event):
+	if Input.is_action_pressed("ToggleFullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
